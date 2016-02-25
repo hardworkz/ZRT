4d719639919144f00022f6a1e5f3b7739db58de2
敲代码mac1号 <qiaodaimamac1hao@192.168.0.111>
2016-02-25 13:44:04 +0800

    Initial Commit

---
+++ ZRT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZRT
+//
+//  Created by 敲代码mac1号 on 16/2/25.
+//  Copyright © 2016年 敲代码mac1号. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZRT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZRT
+//
+//  Created by 敲代码mac1号 on 16/2/25.
+//  Copyright © 2016年 敲代码mac1号. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
